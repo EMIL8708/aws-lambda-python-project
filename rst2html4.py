c725771181328df248747b0cc1f0787d48899cf5
Emil Moldoisaev <moldoisaem@gmail.com>
2024-09-23 13:29:48 -0500

    Update rst2html4.py

--- rst2html4.py
+++ rst2html4.py
@@ -1,4 +1,4 @@
-!/mnt/EMIL8708/aws-lambda-python-project/
+# !/mnt/EMIL8708/aws-lambda-python-project/
 
 # $Id: rst2html4.py 7994 2016-12-10 17:41:45Z milde $
 # Author: David Goodger <goodger@python.org>
