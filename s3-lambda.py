541b205b6d22e53645ba62665fa54e0bc86c17ba
Emil Moldoisaev <moldoisaem@gmail.com>
2024-09-23 10:50:19 -0500

    Create s3-lambda.py

---
+++ s3-lambda.py
@@ -0,0 +1,54 @@
+# import boto3 library
+import boto3 
+import json
+
+def lambda_handler(event, context):
+    # Initialize boto3 s3 client 
+    s3 = boto3.client("s3")
+
+    # Define a variable for a region
+    REGION = "eu-west-2"
+
+    # Create empty list
+    BUCKET_LIST = []
+
+    TAGS = {
+        'TagSet': [
+            {
+                'Key': 'envi',
+                'Value': 'dev'
+            },
+            {
+                'Key': 'managedBy',
+                'Value': 'terraform'
+            },
+            {
+                'Key': 'python',
+                'Value': ‘project’
+ },
+{
+                'Key': 'month',
+                'Value': june
+            },
+        ]
+    }
+
+    # List S3 buckets and save as "response"
+    response = s3.list_buckets()
+
+    # iterate thru response list and print bucket name
+    for bucket in response["Buckets"]:
+        BUCKET_LIST.append(bucket["Name"])
+
+    for bucket in BUCKET_LIST:
+        # Find bucket location
+        bucket_location = s3.get_bucket_location(Bucket=bucket) 
+        bucket_region = bucket_location["LocationConstraint"]
+
+        # if the bucket is in the region
+        if bucket_region == REGION:
+            # print to the screens
+            print("The bucket %s is located in %s" % (bucket, bucket_region))
+            # add tags
+            response = s3.put_bucket_tagging(Bucket=bucket,Tagging=TAGS)
+            print("The following buckets %s are tagged" % bucket)
