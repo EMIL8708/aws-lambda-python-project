c2bb8b8b8bfa8b9e0f30123d7824351634605550
Emil Moldoisaev <moldoisaem@gmail.com>
2024-09-23 11:31:20 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_EMIL8708',
+    version='0.0.16',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/EMIL8708',
+    license='MIT',
+    author='Emil Moldoisaev',
+    author_email='moldoisaem@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
